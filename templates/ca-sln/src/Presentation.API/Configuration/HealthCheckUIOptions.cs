ee1674df727bac448893c66af6620ec2f5a64b9f
Casey <crouse.casey@gmail.com>
2020-05-09 12:09:00 -0700

    feat(Template): Added support for 'dotnet new'
    Closes #1

---
+++ templates/ca-sln/src/Presentation.API/Configuration/HealthCheckUIOptions.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+
+namespace Presentation.API.Configuration
+{
+	/// <summary>
+	/// Provides configuration settings for HealthChecksUI.
+	/// <para>https://github.com/Xabaril/AspNetCore.Diagnostics.HealthChecks</para>
+	/// </summary>
+	public class HealthChecksUIOptions
+	{
+		/// <summary>
+		/// Gets or sets whether the health checks UI is enabled.
+		/// </summary>
+		public bool IsEnabled { get; set; }
+
+		/// <summary>
+		/// Gets or sets the name for the endpoint.
+		/// </summary>
+		public string EndpointName { get; set; } = "API";
+
+		/// <summary>
+		/// Gets or sets the relative endpoint for health checks.
+		/// </summary>
+		public string RelativeEndpoint { get; set; } = "/health";
+
+		/// <summary>
+		/// Gets or sets the health checks evaluation frequency in seconds.
+		/// </summary>
+		public int EvaluationFrequencyInSeconds { get; set; } = 10;
+
+		/// <summary>
+		/// Gets or sets the health checks failure notification frequency in seconds.
+		/// </summary>
+		public int FailureNotificationsFrequencySeconds { get; set; } = 60;
+
+		/// <summary>
+		/// Gets or sets a collection of webhook notifications.
+		/// <para>
+		/// https://github.com/Xabaril/AspNetCore.Diagnostics.HealthChecks/blob/master/doc/webhooks.md
+		/// </para>
+		/// </summary>
+		public IList<WebHookNotification> WebHookNotifications { get; set; } = new List<WebHookNotification>();
+	}
+
+	/// <summary>
+	/// Provides configuration settings for webhook notifications.
+	/// </summary>
+	public class WebHookNotification
+	{
+		/// <summary>
+		/// Gets or sets whether the webhook notification is enabled.
+		/// </summary>
+		public bool IsEnabled { get; set; }
+
+		/// <summary>
+		/// Gets or sets the name.
+		/// </summary>
+		public string? Name { get; set; }
+
+		/// <summary>
+		/// Gets or sets the webhook URI.
+		/// </summary>
+		public string? Uri { get; set; }
+
+		/// <summary>
+		/// Gets or sets the health checks failure notification payload.
+		/// </summary>
+		public string? Payload { get; set; }
+
+		/// <summary>
+		/// Gets or sets the health checks restored notification payload.
+		/// </summary>
+		public string? RestoredPayload { get; set; }
+	}
+}
---
+++ templates/ca-sln/src/Presentation.API/WeatherForecast.cs
@@ -0,0 +1,17 @@
+#nullable enable
+using System;
+#pragma warning disable CS1591 // Missing comments on public members
+
+namespace Presentation.API
+{
+	public class WeatherForecast
+	{
+		public DateTime Date { get; set; }
+
+		public int TemperatureC { get; set; }
+
+		public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+		public string? Summary { get; set; }
+	}
+}
