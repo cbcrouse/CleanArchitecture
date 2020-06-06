9f524749f4bc4a3897fd3fda5fad66d5d7a2992f
Casey <crouse.casey@gmail.com>
2020-06-05 22:42:44 -0700

    fix(Build): dotnet new & Package Downgrade Detected
    * Fixed an issue where the template group identity was the same as the other template causing only one template to show up in the template list as well as not allowing the template to be created.
    * Fixed a package downgrade detected issue by upgrading all packages and ensuring version consistency.
    * Updated the testing script.

--- tools/New-SolutionTest.ps1
+++ tools/New-SolutionTest.ps1
@@ -1,10 +1,11 @@
 
-Set-Location (Join-Path -Path $($PSScriptRoot) -ChildPath "..") | Out-Null
+$defaultLocation = Join-Path -Path $PSScriptRoot -ChildPath ".."
+Set-Location $defaultLocation
 Get-ChildItem -Path . -Recurse -Filter *.nupkg -ErrorAction SilentlyContinue | ForEach-Object { $_ | Remove-Item -Force -ErrorAction SilentlyContinue }
 
-dotnet pack
+dotnet pack 'CleanArchitecture.Templates.csproj'
 
-Set-Location $($PSScriptRoot) | Out-Null
+Set-Location $PSScriptRoot | Out-Null
 $nupkg = Get-ChildItem -Path "../bin/Debug" -Filter CleanArchitecture.Templates.*.nupkg -ErrorAction Stop | Select-Object -First 1
 
 dotnet new -i $nupkg.FullName
@@ -15,13 +16,10 @@ New-Item -Path $path -ItemType Directory | Out-Null
 Set-Location $path | Out-Null
 $solutionName = 'MyApplication'
 
-dotnet new ca-sln --name $($solutionName) --app-name $($solutionName) --secure-port 44399 --port 30356
-
-Set-Location $($solutionName) | Out-Null
-
-dotnet restore "$($solutionName).sln"
-dotnet build "$($solutionName).sln"
-dotnet test "$($solutionName).sln"
-
-Set-Location .. | Out-Null
-Set-Location .. | Out-Null
+# Template Names: ca-sln, ca-sln-sql
+dotnet new ca-sln-sql --name $solutionName --app-name $solutionName --secure-port 44399 --port 34399
+dotnet restore "$solutionName/$solutionName.sln"
+dotnet build "$solutionName/$solutionName.sln" -c Release --no-restore
+$testProjects = Get-ChildItem -Path . -Recurse -Filter *Tests.csproj -ErrorAction SilentlyContinue
+$testProjects | ForEach-Object { dotnet test "$($_.FullName)" --no-restore --no-build --configuration Release }
+Set-Location ../.. | Out-Null
